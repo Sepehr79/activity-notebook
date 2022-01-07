658d026eb3c1c1ea9b23aae663f46441ddfa700b
Sepehr79 <sepehrmsm1379@gmail.com>
2022-01-07 19:01:57 +0330

    Employee rest controller

--- src/main/java/com/sepehr/activity_notebook/controller/EmployeeRestController.java
+++ src/main/java/com/sepehr/activity_notebook/controller/EmployeeRestController.java
@@ -10,47 +10,47 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/notebook/v1")
+@RequestMapping("/notebook/v1/admins")
 @RestController
 @RequiredArgsConstructor
 public class EmployeeRestController {
 
     private final AdminService adminService;
 
-    @GetMapping("/admins/{userName}/employees")
+    @GetMapping("/{userName}/employees")
     public List<Employee> employees(@PathVariable String userName) throws UserNotFoundException {
         Admin admin = adminService.findAdminByUserName(userName);
         return admin.getEmployees();
     }
 
-    @GetMapping("/admins/{userName}/employees/{index}")
+    @GetMapping("/{userName}/employees/{index}")
     public Employee getEmployee(@PathVariable String userName, @PathVariable String index) throws UserNotFoundException {
         Admin admin = adminService.findAdminByUserName(userName);
         return admin.getEmployees().get(Integer.parseInt(index));
     }
 
-    @PostMapping("/admins/{userName}/employees")
+    @PostMapping("/{userName}/employees")
     public Admin addEmployees(@PathVariable String userName, @RequestBody List<Employee> employees) throws UserNotFoundException, DuplicateUserNameException {
         Admin admin = adminService.findAdminByUserName(userName);
         admin.addEmployees(employees);
         return adminService.save(admin);
     }
 
-    @PutMapping("/admins/{userName}/employees")
+    @PutMapping("/{userName}/employees")
     public Admin updateEmployees(@PathVariable String userName ,@RequestBody List<Employee> employees) throws UserNotFoundException, DuplicateUserNameException {
         Admin admin = adminService.findAdminByUserName(userName);
         admin.setEmployees(employees);
         return adminService.save(admin);
     }
 
-    @DeleteMapping("/admins/{userName}/employees")
+    @DeleteMapping("/{userName}/employees")
     public Admin deleteEmployees(@PathVariable String userName) throws UserNotFoundException, DuplicateUserNameException {
         Admin adminByUserName = adminService.findAdminByUserName(userName);
         adminByUserName.getEmployees().clear();
         return adminService.save(adminByUserName);
     }
 
-    @DeleteMapping("/admins/{userName}/employees/{index}")
+    @DeleteMapping("/{userName}/employees/{index}")
     public Admin deleteEmployee(@PathVariable String userName, @PathVariable String index) throws UserNotFoundException, DuplicateUserNameException {
         Admin adminByUserName = adminService.findAdminByUserName(userName);
         adminByUserName.getEmployees().remove(Integer.parseInt(index));
